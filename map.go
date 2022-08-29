9dcd5e601a184f6a2483ea46a1f6071b5457960a
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2022-08-29 09:42:07 +0530

    refactor

--- map.go
+++ map.go
@@ -7,30 +7,6 @@ import (
 	"unsafe"
 )
 
-const (
-	// hash input allowed sizes
-	byteSize = 1 << iota
-	wordSize
-	dwordSize
-	qwordSize
-	owordSize
-
-	// DefaultSize is the default size for a zero allocated map
-	DefaultSize = 256
-
-	// MaxFillRate is the maximum fill rate for the slice before a resize  will happen.
-	MaxFillRate = 50
-
-	// intSizeBytes is the size in byte of an int or uint value.
-	intSizeBytes = strconv.IntSize >> 3
-)
-
-// indicates resizing operation status enums
-const (
-	notResizing uint32 = iota
-	resizingInProgress
-)
-
 type (
 	// allowed map key types constraint
 	hashable interface {
@@ -38,36 +14,36 @@ type (
 	}
 
 	hashMapData[K hashable, V any] struct {
-		Keyshifts uintptr        // Pointer size - log2 of array size, to be used as index in the data array
-		count     atomic.Uintptr // count of filled elements in the slice
-		data      unsafe.Pointer // pointer to slice data array
+		Keyshifts uintptr        //  array_size - log2(array_size)
+		count     atomic.Uintptr // number of filled items
+		data      unsafe.Pointer // pointer to array
 		index     []*element[K, V]
 	}
 
-	// HashMap implements a read optimized hash map.
+	// HashMap implements the concurrent hashmap
 	HashMap[K hashable, V any] struct {
-		listHead *element[K, V] // key sorted linked list of elements
+		listHead *element[K, V] // Harris lock-free list of elements in ascending order of hash
 		hasher   func(K) uintptr
-		Datamap  atomic.Pointer[hashMapData[K, V]] // pointer to a map instance that gets replaced if the map resizes
+		Datamap  atomic.Pointer[hashMapData[K, V]] // atomic.Pointer for safe access even during resizing
 		resizing atomic.Uint32
 		numItems atomic.Uintptr
 	}
 )
 
-// New returns a new HashMap instance with an optional specific initialization size.
+// New returns a new HashMap instance with an optional specific initialization size
 func New[K hashable, V any](size ...uintptr) *HashMap[K, V] {
 	m := &HashMap[K, V]{listHead: newListHead[K, V]()}
 	m.numItems.Store(0)
 	if len(size) > 0 {
 		m.allocate(size[0])
 	} else {
-		m.allocate(DefaultSize)
+		m.allocate(defaultSize)
 	}
 	m.setDefaultHasher()
 	return m
 }
 
-// returns the index of a hash key, returns `nil` if absent
+// indexElement returns the index of a hash key, returns `nil` if absent
 func (mapData *hashMapData[K, V]) indexElement(hashedKey uintptr) *element[K, V] {
 	index := hashedKey >> mapData.Keyshifts
 	ptr := (*unsafe.Pointer)(unsafe.Pointer(uintptr(mapData.data) + index*intSizeBytes))
@@ -80,7 +56,8 @@ func (mapData *hashMapData[K, V]) indexElement(hashedKey uintptr) *element[K, V]
 	return item
 }
 
-// Del deletes the key from the map.
+// Del deletes the key from the map
+// does nothing if key is absemt
 func (m *HashMap[K, V]) Del(key K) {
 	h := m.hasher(key)
 
@@ -116,7 +93,8 @@ loop:
 	}
 }
 
-// Get retrieves an element from the map under given hash key.
+// Get retrieves an element from the map
+// returns `false“ if element is absent
 func (m *HashMap[K, V]) Get(key K) (value V, ok bool) {
 	h := m.hasher(key)
 	// inline search
@@ -135,8 +113,9 @@ func (m *HashMap[K, V]) Get(key K) (value V, ok bool) {
 	return
 }
 
-// Set sets the value under the specified key to the map. An existing item for this key will be overwritten.
-// If a resizing operation is happening concurrently while calling Set, the item might show up in the map only after the resize operation is finished.
+// Set tries to update an element if key is present else it inserts a new element
+// If a resizing operation is happening concurrently while calling Set()
+// then the item might show up in the map only after the resize operation is finished
 func (m *HashMap[K, V]) Set(key K, value V) {
 	h, valPtr := m.hasher(key), &value
 	var (
@@ -146,7 +125,7 @@ func (m *HashMap[K, V]) Set(key K, value V) {
 	for {
 		data := m.Datamap.Load()
 		if data == nil {
-			m.Grow(DefaultSize)
+			m.Grow(defaultSize)
 			continue // read mapdata and slice item again
 		}
 		existing := data.indexElement(h)
@@ -165,38 +144,36 @@ func (m *HashMap[K, V]) Set(key K, value V) {
 	}
 }
 
-// adds an item to the index if needed and returns the new item counter if it changed, otherwise 0
+// addItemToIndex adds an item to the index if needed and returns the new item counter if it changed, otherwise 0
 func (mapData *hashMapData[K, V]) addItemToIndex(item *element[K, V]) uintptr {
 	index := item.keyHash >> mapData.Keyshifts
 	ptr := (*unsafe.Pointer)(unsafe.Pointer(uintptr(mapData.data) + index*intSizeBytes))
-
-	for { // loop until the smallest key hash is in the index
-		element := (*element[K, V])(atomic.LoadPointer(ptr)) // get the current item in the index
-		if element == nil {                                  // no item yet at this index
+	for {
+		element := (*element[K, V])(atomic.LoadPointer(ptr))
+		if element == nil {
 			if atomic.CompareAndSwapPointer(ptr, nil, unsafe.Pointer(item)) {
 				return mapData.count.Add(1)
 			}
-			continue // a new item was inserted concurrently, retry
+			continue
 		}
 
 		if item.keyHash < element.keyHash {
-			// the new item is the smallest for this index?
 			if !atomic.CompareAndSwapPointer(ptr, unsafe.Pointer(element), unsafe.Pointer(item)) {
-				continue // a new item was inserted concurrently, retry
+				continue
 			}
 		}
 		return 0
 	}
 }
 
+// fillIndexItems re-indexes the map given the latest state of the linked list
 func (m *HashMap[K, V]) fillIndexItems(mapData *hashMapData[K, V]) {
 	first := m.listHead
 	item := first
 	lastIndex := uintptr(0)
-
 	for item != nil {
 		index := item.keyHash >> mapData.Keyshifts
-		if item == first || index != lastIndex { // store item with smallest hash key for every index
+		if item == first || index != lastIndex {
 			mapData.addItemToIndex(item)
 			lastIndex = index
 		}
@@ -204,17 +181,17 @@ func (m *HashMap[K, V]) fillIndexItems(mapData *hashMapData[K, V]) {
 	}
 }
 
-// ForEach iterates over key-value pairs and executes the lambda provided for each such pair.
+// ForEach iterates over key-value pairs and executes the lambda provided for each such pair
 func (m *HashMap[K, V]) ForEach(lambda func(K, V)) {
 	for item := m.listHead.next(); item != nil; item = item.next() {
 		lambda(item.key, *item.value.Load())
 	}
 }
 
-// Grow resizes the hashmap to a new size, gets rounded up to next power of 2.
-// To double the size of the hashmap use newSize 0.
-// This function returns immediately, the resize operation is done in a goroutine.
-// No resizing is done in case of another resize operation already being in progress.
+// Grow resizes the hashmap to a new size, gets rounded up to next power of 2
+// To double the size of the hashmap use newSize 0
+// This function returns immediately, the resize operation is done in a goroutine
+// No resizing is done in case of another resize operation already being in progress
 func (m *HashMap[K, V]) Grow(newSize uintptr) {
 	if m.resizing.CompareAndSwap(notResizing, resizingInProgress) {
 		m.grow(newSize, true)
@@ -226,17 +203,18 @@ func (m *HashMap[K, V]) SetHasher(hs func(K) uintptr) {
 	m.hasher = hs
 }
 
-// Len returns the number of key-value pairs within the map.
+// Len returns the number of key-value pairs within the map
 func (m *HashMap[K, V]) Len() uintptr {
 	return m.numItems.Load()
 }
 
-// Fillrate returns the fill rate of the map as an percentage integer.
+// Fillrate returns the fill rate of the map as an percentage integer
 func (m *HashMap[K, V]) Fillrate() uintptr {
 	data := m.Datamap.Load()
 	return (data.count.Load() * 100) / uintptr(len(data.index))
 }
 
+// allocate map with the given size
 func (m *HashMap[K, V]) allocate(newSize uintptr) {
 	if m.resizing.CompareAndSwap(notResizing, resizingInProgress) {
 		m.grow(newSize, false)
@@ -260,50 +238,23 @@ func (m *HashMap[K, V]) grow(newSize uintptr, loop bool) {
 
 		newdata := &hashMapData[K, V]{
 			Keyshifts: strconv.IntSize - log2(newSize),
-			data:      unsafe.Pointer(header.Data), // use address of slice data storage
+			data:      unsafe.Pointer(header.Data),
 			index:     index,
 		}
 
-		m.fillIndexItems(newdata) // initialize new index slice with longer keys
+		m.fillIndexItems(newdata) // re-index with longer and more widespread keys
 
 		m.Datamap.Store(newdata)
 
-		m.fillIndexItems(newdata) // make sure that the new index is up-to-date with the current state of the linked list
+		m.fillIndexItems(newdata) // re-index once again just to be safe
 
 		if !loop {
 			return
 		}
 
-		// check if a new resize needs to be done already
 		if !resizeNeeded(newSize, uintptr(m.Len())) {
 			return
 		}
 		newSize = 0 // 0 means double the current size
 	}
-}
-
-func resizeNeeded(length, count uintptr) bool {
-	return (count*100)/length > MaxFillRate
-}
-
-// roundUpPower2 rounds a number to the next power of 2.
-func roundUpPower2(i uintptr) uintptr {
-	i--
-	i |= i >> 1
-	i |= i >> 2
-	i |= i >> 4
-	i |= i >> 8
-	i |= i >> 16
-	i |= i >> 32
-	i++
-	return i
-}
-
-// log2 computes the binary logarithm of x, rounded up to the next integer.
-func log2(i uintptr) uintptr {
-	var n, p uintptr
-	for p = 1; p < i; p += p {
-		n++
-	}
-	return n
 }
