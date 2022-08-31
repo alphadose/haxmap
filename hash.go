f7fd3700af2eb177debb26de67e103e44e9e9967
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2022-08-31 19:25:24 +0530

    fix string hashing

--- hash.go
+++ hash.go
@@ -51,59 +51,6 @@ const (
 
 var prime1v = prime1
 
-// defaultSum implements xxHash for 64 bit systems
-func defaultSum(b []byte) uintptr {
-	n := len(b)
-	var h uint64
-
-	if n >= 32 {
-		v1 := prime1v + prime2
-		v2 := prime2
-		v3 := uint64(0)
-		v4 := -prime1v
-		for len(b) >= 32 {
-			v1 = round(v1, u64(b[0:8:len(b)]))
-			v2 = round(v2, u64(b[8:16:len(b)]))
-			v3 = round(v3, u64(b[16:24:len(b)]))
-			v4 = round(v4, u64(b[24:32:len(b)]))
-			b = b[32:len(b):len(b)]
-		}
-		h = rol1(v1) + rol7(v2) + rol12(v3) + rol18(v4)
-		h = mergeRound(h, v1)
-		h = mergeRound(h, v2)
-		h = mergeRound(h, v3)
-		h = mergeRound(h, v4)
-	} else {
-		h = prime5
-	}
-
-	h += uint64(n)
-
-	i, end := 0, len(b)
-	for ; i+8 <= end; i += 8 {
-		k1 := round(0, u64(b[i:i+8:len(b)]))
-		h ^= k1
-		h = rol27(h)*prime1 + prime4
-	}
-	if i+4 <= end {
-		h ^= uint64(u32(b[i:i+4:len(b)])) * prime1
-		h = rol23(h)*prime2 + prime3
-		i += 4
-	}
-	for ; i < end; i++ {
-		h ^= uint64(b[i]) * prime5
-		h = rol11(h) * prime1
-	}
-
-	h ^= h >> 33
-	h *= prime2
-	h ^= h >> 29
-	h *= prime3
-	h ^= h >> 32
-
-	return uintptr(h)
-}
-
 func u64(b []byte) uint64 { return binary.LittleEndian.Uint64(b) }
 func u32(b []byte) uint32 { return binary.LittleEndian.Uint32(b) }
 
@@ -138,9 +85,59 @@ func (m *HashMap[K, V]) setDefaultHasher() {
 
 	switch any(*new(K)).(type) {
 	case string:
+		// use default xxHash algorithm for key of any size for golang string data type
 		m.hasher = func(key K) uintptr {
 			sh := (*reflect.StringHeader)(unsafe.Pointer(&key))
-			return defaultSum(unsafe.Slice((*byte)(unsafe.Pointer(&key)), sh.Len))
+			b := unsafe.Slice((*byte)(unsafe.Pointer(sh.Data)), sh.Len)
+			n := sh.Len
+			var h uint64
+
+			if n >= 32 {
+				v1 := prime1v + prime2
+				v2 := prime2
+				v3 := uint64(0)
+				v4 := -prime1v
+				for len(b) >= 32 {
+					v1 = round(v1, u64(b[0:8:len(b)]))
+					v2 = round(v2, u64(b[8:16:len(b)]))
+					v3 = round(v3, u64(b[16:24:len(b)]))
+					v4 = round(v4, u64(b[24:32:len(b)]))
+					b = b[32:len(b):len(b)]
+				}
+				h = rol1(v1) + rol7(v2) + rol12(v3) + rol18(v4)
+				h = mergeRound(h, v1)
+				h = mergeRound(h, v2)
+				h = mergeRound(h, v3)
+				h = mergeRound(h, v4)
+			} else {
+				h = prime5
+			}
+
+			h += uint64(n)
+
+			i, end := 0, len(b)
+			for ; i+8 <= end; i += 8 {
+				k1 := round(0, u64(b[i:i+8:len(b)]))
+				h ^= k1
+				h = rol27(h)*prime1 + prime4
+			}
+			if i+4 <= end {
+				h ^= uint64(u32(b[i:i+4:len(b)])) * prime1
+				h = rol23(h)*prime2 + prime3
+				i += 4
+			}
+			for ; i < end; i++ {
+				h ^= uint64(b[i]) * prime5
+				h = rol11(h) * prime1
+			}
+
+			h ^= h >> 33
+			h *= prime2
+			h ^= h >> 29
+			h *= prime3
+			h ^= h >> 32
+
+			return uintptr(h)
 		}
 	case int, uint, uintptr:
 		switch intSizeBytes {
